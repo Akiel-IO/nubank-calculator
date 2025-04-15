19d6bf3a3346a999a3f2349a723477d2a731f453
Akiel <daniel.cs_@outlook.com>
2025-04-15 16:48:17 -0500

    Setup remote repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 import com.skandinavia.service.DailyInterestService;
 
 import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,13 +10,5 @@ public class Main {
         System.out.println(df
                 .format(dailyInterestService
                 .calculateDailyReturn(1000000.00, dailyInterestService.getDailyInterestConversion())));
-
-        List<String> names = Arrays.asList("Daniel", "Ana", "Salomon", "Karelyz", "Karelyz");
-        names.stream()
-                .distinct()
-
-
-        System.out.println(names);
-
     }
 }
